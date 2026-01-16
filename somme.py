277342a03a9f54c406466de97ac03b78f9638844
MaxGnrd <maxguenard09@gmail.com>
2026-01-16 14:55:41 +0100

    Add somme.py to calculate the sum of two numbers

---
+++ somme.py
@@ -0,0 +1,7 @@
+nombre_1=input("Quel est le premier nombre ?")
+nombre_2=input("Quel est le deuxième nombre ?")
+nombre_1=int(nombre_1)
+nombre_2=int(nombre_2)
+print ("La somme {nombre_1} et {nombre_2} vaut :")
+print (f"{nombre_1} + {nombre_2} = {nombre_1+nombre_2}")
+

ab4593ce0876b8429446f959ac383ba8eddfb423
MaxGnrd <maxguenard09@gmail.com>
2026-01-23 15:29:51 +0100

    Implement age calculation based on birth years
    Fix syntax errors and improve age calculation.

---
+++ age.py
@@ -0,0 +1,26 @@
+import datetime
+now=datetime.date.yoday(2026).year
+
+nom1=input("quel est ton nom?")
+annee_1=int(input("En quelle année est-tu né(e)?")
+nom2=input("comment s'apelle ton voisin?")
+anee_2=int(input("En quelle année est-il né?")
+
+annee_1=int(annee_1)
+annee_2=int(annee_2)
+age_1=now-annee_1
+age_2=now-annee_2
+
+print(f"{nom_1} a {age_1} ans.")
+print(f"{nom_2} a {age_2} ans.")
+
+if age_1 > age_2 :
+           print(f"{nom_1} a {age_1-age_2} ans de plus que {nom_2}.")
+
+elif age_2 > age_1 :
+           print(f"{nom_2} a {age_2-age_1} ans de plus que {nom_1}.")
+
+
+
+elif age_1==age_2 :
+           print(f"{nom_1} et {nom_2} ont le meme age.")

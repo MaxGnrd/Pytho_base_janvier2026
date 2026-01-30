cf5f753411e2ac791d151b9fa9878a2d4531f381
MaxGnrd <maxguenard09@gmail.com>
2026-01-30 14:23:54 +0100

    Fix datetime import and variable names in age.py

--- age.py
+++ age.py
@@ -1,10 +1,10 @@
-import datetime
-now=datetime.date.yoday(2026).year
+from datetime import datetime
+now=datetime.today().year
 
-nom1=input("quel est ton nom?")
+nom_1=input("quel est ton nom?")
 annee_1=input("En quelle année est-tu né(e)?")
-nom2=input("comment s'apelle ton voisin?")
-anee_2=input("En quelle année est-il né?")
+nom_2=input("comment s'apelle ton voisin?")
+annee_2=input("En quelle année est-il né?")
 
 annee_1=int(annee_1)
 annee_2=int(annee_2)
@@ -15,12 +15,12 @@ print(f"{nom_1} a {age_1} ans.")
 print(f"{nom_2} a {age_2} ans.")
 
 if age_1 > age_2 :
-           print(f"{nom_1} a {age_1-age_2} ans de plus que {nom_2}.")
+    print(f"{nom_1} a {age_1-age_2} ans de plus que {nom_2}.")
 
 elif age_2 > age_1 :
-           print(f"{nom_2} a {age_2-age_1} ans de plus que {nom_1}.")
+    print(f"{nom_2} a {age_2-age_1} ans de plus que {nom_1}.")
 
 
 
 elif age_1==age_2 :
-           print(f"{nom_1} et {nom_2} ont le meme age.")
+    print(f"{nom_1} et {nom_2} ont le meme age.")

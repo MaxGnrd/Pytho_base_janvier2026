a21f6c1e7248b187ab3605c51ef129f8f990bfed
MaxGnrd <maxguenard09@gmail.com>
2026-02-06 14:22:22 +0100

    Refactor prompts and output messages in racine.py

--- racine.py
+++ racine.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
 print("Bonjour et bienvenue dans ce programme de calcul des racines d'une fonction du deuxième degré.")
-print("Dans l'équation y=ax²+bx+c,")
+print("Dans la fonction y=ax²+bx+c,")
 
-a=input("Quelle est la valeur de a? ")
-b=input("Quelle est la valeur de b? ")
-c=input("Quelle est la valeur de c? ")
+a=input("Quelle est la valeur de a ? ")
+b=input("Quelle est la valeur de b ? ")
+c=input("Quelle est la valeur de c ? ")
 
 a=float(a)
 b=float(b)
@@ -14,22 +14,22 @@ c=float(c)
 delta=b**2-4*a*c
 
 
-
-
-
-if delta < 0:
-    print("Delta n'as malheureusement pas de racines!")
+if delta < 0 :
+    
+    print("Il n'y a pas de racine à cette fonction")
      
-elif delta== 0 :
-    racine_1=-b/(2 * a)
-    print(f"Delta n'as qu'une racine qui vaut {racine_1}")
-      
-elif delta > 0:
-    racine_2 = (-b + sqrt(delta)) / (2 * a)
-    racine_3= (-b - sqrt(delta)) / (2 * a)
+elif delta == 0 :
+    x=(-b)/(2*a)
+    print("Il y a une racine à cette fonction")
+    print(f"Sa valeur est de : x={round(x,4)}")  
 
-    print(f"Delta a 2 racines qui valent {round(racine_2,5)} et {racine_3}")
-     
-else :
-    print("error")
+elif delta > 0 :
+    x1 = (-b + sqrt(delta)) / (2 * a)
+    x2 = (-b - sqrt(delta)) / (2 * a)
 
+    print("Il y a deux racines à cette fonction.")
+    print(f"la valeur de la première est de : x1={round(x1,4)}")
+    print(f"la valeur de la première est de : x2={round(x2,4)}")
+    
+    print("Au revoir !")
+ 

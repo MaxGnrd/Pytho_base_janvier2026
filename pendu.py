4946c4fddb9d2101fa4f0603b8a1a85bd6299847
MaxGnrd <maxguenard09@gmail.com>
2026-03-13 14:05:24 +0100

    Enhance code readability with comments
    Added comments to clarify code functionality.

--- pendu.py
+++ pendu.py
@@ -1,14 +1,16 @@
 mot="anniversaire"
 list_lettres_mot=[]
+#calcul du nombre de lettres différentes dans le mot
 for lettre in mot:
     if lettre not in list_lettres_mot:
         list_lettres_mot.append(lettre)
 tentatives_autorisees=len(list_lettres_mot)+3
 
-
+#
 list_propositions=[]
+#on commence la boucle principale au sein de laquelle on va proposer des lettres
 for i in range (tentatives_autorisees):
-    for lettre in mot:
+    for lettre in mot: #pour chaques lettres du mot ,on affiche soit la lettre soit le caractère "_"
         if lettre in list_propositions :
             print( lettre,end="")
         else:

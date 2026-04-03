5fae74db707570ec295d0d1c574a6435dd2bc372
MaxGnrd <maxguenard09@gmail.com>
2026-04-03 15:26:39 +0200

    Refactor area calculation functions and input handling

--- aires.py
+++ aires.py
@@ -5,8 +5,21 @@ def menu():
     print("Pour calculer l'aire d'un cercle, tapez 3")
     print("Pour quitter le programme, tapez 0")
     choix=input("Votre choix")
-    choix=int(choix)
-    #on met "int" pour que ce qu'on écrive soit considéré pas comme un caractère mais le convertit en nombre entier.
+    #choix=int(choix)#on met "int" pour que ce qu'on écrive soit considéré pas comme un caractère mais le convertit en nombre entier.
+    
+    
+    valid=False
+    while not valid :
+        try:
+            choix=int(choix)#pour convertir la string en un nombre entier on écrit int()
+            if choix in [0,1,2,3]:
+                valid=True
+            else:
+                raise
+        except:
+            print("Veuillez répondre par 0,1,2 ou 3")
+            choix=input("Votre choix : ")
+            
 
     if choix==1:
         aire_carre()
@@ -24,18 +37,27 @@ def quit():
 def aire_carre():
     cote=input("Quelle est la longueur du côté de votre carré ? ")
     cote=float(cote)
-    aire=cote**2
-    print(f"L'aire de votre carré vaut : {aire}")
+    aire_carre=cote**2
+    print(f"L'aire de votre carré vaut : {aire_carre}")
     menu()
     #menu() pour qu'il affiche la réponse et direct après réaffiche le menu pour recommencer ; à l'infini
     #"f"=string de formatage dans lequel on place la valeur de la variable : le chiffre du côté du carré qu'on a donné
 
 def aire_cercle():
-    def aire_cercle():
     rayon=input("Quelle est la longueur du rayon de votre cercle ? ")
     rayon=float(rayon)
-    aire=math.py*rayon**2
-    print(f"L'aire de votre cercle vaut : {aire}")
+    aire_cercle=math.pi*rayon**2
+    print(f"L'aire de votre cercle vaut : {round(aire_cercle,4)}")
+    menu()
+
+
+def aire_triangle():
+    base=input("Quelle est la longueur de la base de votre triangle ? ")
+    base=float(base)
+    hauteur=input("Quelle est la hauteur de votre triangle ?")
+    hauteur=float(hauteur)
+    aire_triangle=(base*hauteur)/2
+    print(f"L'aire de votre triangle vaut : {round(aire_triangle,4)}")
     menu()
 
 
